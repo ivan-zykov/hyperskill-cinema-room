f2cefacfc8a4161afe48357ca5d278d4389c16aa
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-09-30 15:02:22 +0200

    Add service

--- src/cinema/CinemaController.kt
+++ src/cinema/CinemaController.kt
@@ -1,18 +1,25 @@
 package cinema
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class CinemaController {
+class CinemaController @Autowired constructor(val cinemaService: CinemaService) {
 
     @GetMapping("/health")
     fun health(): ResponseEntity<String> = ResponseEntity("", HttpStatus.OK)
 
     @GetMapping("/seats")
     fun seats(): ResponseEntity<SeatsAvailable> {
-        TODO("not implemented yet")
+        val seats = cinemaService.getSeats()
+
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(seats)
     }
 }
