9558bf587e44b9458e60026578ced01a7acfe126
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-09-30 14:24:37 +0200

    Add health endpoint

---
+++ src/cinema/CinemaController.kt
@@ -0,0 +1,13 @@
+package cinema
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CinemaController {
+
+    @GetMapping("/health")
+    fun health(): ResponseEntity<String> = ResponseEntity("", HttpStatus.OK)
+}
