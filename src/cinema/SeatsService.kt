9b663c4de058c6c14a6e74c00dc409b017ea77f1
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-10-20 15:49:53 +0200

    Replace seat ID with UUID

--- src/cinema/SeatsService.kt
+++ src/cinema/SeatsService.kt
@@ -1,6 +1,8 @@
 package cinema
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
 
@@ -8,14 +10,14 @@ private const val NUM_OF_COLUMNS = 9
 private const val NUM_OF_ROWS = NUM_OF_COLUMNS
 
 @Service
-class SeatsService {
-    private val seats: ConcurrentMap<Int, Seat> = ConcurrentHashMap()
+class SeatsService @Autowired constructor(tokenService: TokenService) {
+    private val seats: ConcurrentMap<UUID, Seat> = ConcurrentHashMap()
 
     init {
         (1..NUM_OF_ROWS).forEach { row ->
             (1..NUM_OF_COLUMNS).forEach { column ->
                 val price = if (row <= 4) 10 else 8
-                val id = computeSeatId(row, column)
+                val id: UUID = tokenService.generateToken()
                 val seat = Seat(
                     row = row,
                     column = column,
@@ -51,13 +53,12 @@ class SeatsService {
         row: Int,
         column: Int
     ): Seat {
-        val id = computeSeatId(row = row, column = column)
-        val seat = seats[id]
+        val seat: Seat? = seats.values.asSequence().filter { seat ->
+            seat.row == row && seat.column == column
+        }.firstOrNull()
 
         checkNotNull(seat) { "The number of a row or a column is out of bounds!" }
 
         return seat
     }
-
-    private fun computeSeatId(row: Int, column: Int) = row * 10 + column
 }
