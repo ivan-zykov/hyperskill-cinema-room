06c74e3ea57453fae76f857c7fe8f6e1480aabee
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-10-21 14:26:12 +0200

    Remove equals() and hashCode() from Seat class

--- src/cinema/Domain.kt
+++ src/cinema/Domain.kt
@@ -2,31 +2,13 @@ package cinema
 
 import java.util.*
 
-data class Seat(
+class Seat(
     val row: Int,
     val column: Int,
     val price: Int,
     var isTaken: Boolean = false,
     var token: UUID? = null,
 ) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Seat
-
-        if (row != other.row) return false
-        if (column != other.column) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = row
-        result = 31 * result + column
-        return result
-    }
-
     fun reserveWith(token: UUID) {
         isTaken = true
         this.token = token
